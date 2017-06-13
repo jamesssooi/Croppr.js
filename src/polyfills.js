28b1e234afe4064217084635effa930ca106071f
James Ooi <wengteikooi@gmail.com>
2017-06-13 16:19:37 +0800

    Add CustomEvent polyfill
    This commit adds a polyfill for CustomEvent, which is not supported in
    IE11 and below. Thus preventing Croppr from working on those browsers.

--- src/polyfills.js
+++ src/polyfills.js
@@ -28,6 +28,23 @@
         };
 }());
 
+// CustomEvents polyfill
+(function () {
+
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
 // MouseEvents polyfill
 (function (window) {
   try {
