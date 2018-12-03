ecee06ae99a90700574f173cbbfc64bd908f82c4
James Ooi <wengteikooi@gmail.com>
2018-12-03 14:47:45 +0800

    Update build process

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -2,16 +2,16 @@ import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 import cleanup from 'rollup-plugin-cleanup';
 
-var banner = `/**
+const banner = `/**
  * Croppr.js
  * https://github.com/jamesssooi/Croppr.js
  * 
  * A JavaScript image cropper that's lightweight, awesome, and has
  * zero dependencies.
  * 
- * (C) 2017 James Ooi. Released under the MIT License.
+ * (C) 2018 James Ooi. Released under the MIT License.
  */
-`
+`;
 
 export default {
   entry: 'src/index.js',
@@ -24,6 +24,6 @@ export default {
   ],
   format: 'umd',
   moduleName: 'Croppr',
-  dest: 'dist/croppr.js',
+  dest: 'build/croppr.js',
   banner: banner
 };
