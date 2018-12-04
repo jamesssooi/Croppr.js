a822129862de42f71a8512a919937aa196edc668
James Ooi <wengteikooi@gmail.com>
2018-12-04 10:26:21 +0800

    Convert croppr.js to TypeScript

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,33 @@
+import Croppr from './croppr';
+
+export interface CropprOptions {
+  aspectRatio?: number;
+  maxSize?: Size;
+  minSize?: Size;
+  startSize?: Size;
+  onCropStart?(data: CropValue): void;
+  onCropMove?(data: CropValue): void;
+  onCropEnd?(data: CropValue): void;
+  onInitialize?(instance: Croppr): void;
+  returnMode?: 'real' | 'ratio' | 'raw';
+}
+
+/**
+ * Represents a width and a height value. May optionally include a third value
+ * denoting whether this value is in 'px' or in '%'.
+ */
+export interface Size extends Array<number | string> {
+  0: number
+  1: number
+  2?: 'px' | '%'
+}
+
+export type CropValue = {
+  x: number
+  y: number
+  width: number
+  height: number
+}
 
 /**
  * Represents an x and y position.
@@ -6,7 +36,6 @@ export interface Point extends Array<number> {
   0: number
   1: number
 }
-
 
 /**
  * Represents the side of the crop region that is to be affected by this handle.
