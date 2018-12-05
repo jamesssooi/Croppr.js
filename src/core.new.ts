50acd7c717c5f562800395160439066c6251fc4e
James Ooi <wengteikooi@gmail.com>
2018-12-05 15:45:55 +0800

    Minor fixes

--- src/core.new.ts
+++ src/core.new.ts
@@ -1,10 +1,21 @@
 import { CropprOptions } from "./types";
+import EventBus from './lib/event-bus';
 import * as CONST from './lib/constants';
 import * as Utils from './utils';
+import * as DOMBuilders from './dom';
+import Box from "./lib/box";
 
 class Core {
 
   private handles: { element: HTMLElement, constraints: number[] }[];
+  private eventBus: EventBus;
+  private _oldElement: Element;
+
+  /**
+   * The crop region's internal model. Any modification to the crop region
+   * should be made to this `box` variable.
+   */
+  private box: Box;
 
   /**
    * @constructor
@@ -31,34 +42,26 @@ class Core {
       return;
     }
 
+    this.eventBus = new EventBus();
+
+    // Create DOM
     const imageSrc = element.getAttribute('src');
     const imageAlt = element.getAttribute('alt');
-    const rootDOM = this.constructDOMTree(imageSrc, imageAlt);
+    const rootDOM = this.constructDOMTree(this.eventBus, imageSrc, imageAlt);
     element.parentElement.replaceChild(rootDOM, element);
+    this._oldElement = element;
   }
 
   /**
    * Constructs and returns Croppr's DOM elements.
    */
-  private constructDOMTree(imageSrc: string, imageAlt: string) {
+  private constructDOMTree(eventBus: EventBus, imageSrc: string, imageAlt: string) {
     const root = DOMBuilders.createRootDOM();
     DOMBuilders.createImageDOM(root, imageSrc, imageAlt);
     DOMBuilders.createCropRegionDOM(root);
     DOMBuilders.createOverlayDOM(root);
     DOMBuilders.createHandlesDOM(root);
     return root;
-  }
-
-  /**
-   * Create and returns default handles' DOM elements;
-   */
-  private createHandles() {
-    return CONST.DEFAULT_HANDLES.map(handle => {
-      const element = document.createElement('div');
-      element.className = 'croppr-handle';
-      element.style.cursor = handle.cursor;
-      return { element, constraints: handle.constraints }
-    });
   }
 
 }
